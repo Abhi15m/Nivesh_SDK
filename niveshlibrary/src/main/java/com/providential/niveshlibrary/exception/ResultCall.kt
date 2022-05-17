0f8c626488ef364c3a73e2d9d045948152a59e7d
Abhinav <abhinav.kumar@nivesh.com>
2022-05-17 13:20:58 +0530

    update

--- niveshlibrary/src/main/java/com/providential/niveshlibrary/exception/ResultCall.kt
+++ niveshlibrary/src/main/java/com/providential/niveshlibrary/exception/ResultCall.kt
@@ -39,10 +39,13 @@ class ResultCall<T>(private val delegate: Call<T>) : Call<Result<T>> {
                 }
 
                 override fun onFailure(call: Call<T>, t: Throwable) {
-                    val errorMessage = when (t) {
-                        is IOException -> "No internet connection"
-                        is HttpException -> "Something went wrong!"
-                        else -> t.localizedMessage
+                    val exception = t as HttpException
+                    val errorMessage = when (exception.code()) {
+                        401 -> "Unauthorized"
+                        403 -> "Token expired"
+                        503 -> "No internet connection"
+
+                        else -> "Something went wrong!"
                     }
                     callback.onResponse(
                         this@ResultCall,
